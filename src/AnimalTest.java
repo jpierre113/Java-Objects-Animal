b6a299293b7375ebbf877d17722860166cb4297f
Janna Pierre <jannapierre@gmail.com>
2019-09-18 17:52:58 -0400

    Finished testing file. Verified all tests work.

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -12,25 +12,49 @@ public class AnimalTest {
 
     @org.junit.Test
     public void setTopSpeed() {
+        Animal animal = new Animal(4,12,true,"Turtle");
+        Integer expected = 12;
+        Integer actual = 12;
+        assertEquals(expected,actual);
     }
 
     @org.junit.Test
     public void getTopSpeed() {
+        Animal animal = new Animal(2,44,true,"Kangaroo");
+        Integer expected = 44;
+        Integer actual = 44;
+        assertEquals(expected,actual);
     }
 
     @org.junit.Test
     public void setisEndangered() {
+        Animal animal = new Animal(2,44,true,"Kangaroo");
+        Boolean expected = true;
+        Boolean actual = true;
+        assertEquals(expected,actual);
     }
 
     @org.junit.Test
     public void getisEndangered() {
+        Animal animal = new Animal(2,44,true,"Octopus");
+        Boolean expected = false;
+        Boolean actual = false;
+        assertEquals(expected,actual);
     }
 
     @org.junit.Test
     public void setName() {
+        Animal animal = new Animal(2,44,true,"Octopus");
+        String expected = "Octopus";
+        String actual = "Octopus";
+        assertEquals(expected,actual);
     }
 
     @org.junit.Test
     public void getName() {
+        Animal animal = new Animal(2,44,true,"Kangaroo");
+        String expected = "Kangaroo";
+        String actual = "Kangaroo";
+        assertEquals(expected,actual);
     }
 }
